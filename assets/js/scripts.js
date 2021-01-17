3e2d515499c0e30f778957fb156ed82f3cd29d96
Terrence Chan <terrencechan.writer@gmail.com>
2021-01-17 06:33:21 -0500

    Pulls most current weather information. Missing UV index data.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,5 @@
 var cityName;
 var searchValue = document.getElementById('search');
-var currentData;
 
 var init = function () {
     searchValue.addEventListener('submit', function () {
@@ -12,17 +11,25 @@ var init = function () {
 
 // Fetch request for CURRENT WEATHER
 function callWeather(calllback) {
-    currentData = fetch('https://api.openweathermap.org/data/2.5/weather?q=Waterloo&appid=569731c1a4f5965656b996cf8ec76ae7')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Waterloo&appid=569731c1a4f5965656b996cf8ec76ae7&units=metric')
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            document.getElementById('current-weather').innerText = data.weather[0].main;
+            let today = new Date();
+            let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+            document.getElementById('city-name').innerText = data.name;
+            document.getElementById('current-date').innerText = date;
+            document.getElementById('current-weather').innerText = "There is " + data.weather[0].description + " outside.";
+            document.getElementById('current-temp').innerText = "Temperature is " + data.main.temp + "°C";
+            document.getElementById('current-humidity').innerText = "Humidity is " + data.main.humidity + "%";
+            document.getElementById('current-wind').innerText = "Current wind speeds are " + data.wind.speed + "km/h.";
+            document.getElementById('current-uv').innerText = "The UV index is:";
+            document.getElementById('current-icon').innerHTML = "<img src=\"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png\">";
         })
         .catch(function (error) {
             console.log('Request failed', error)
         });
-    console.log(currentData);
 }
 
 
