3495805a1aad47a23c5d3ed1aece997c274eb453
Terrence Chan <terrencechan.writer@gmail.com>
2021-01-17 12:42:58 -0500

    Displays five day forecast.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,7 +17,7 @@ function callWeather(calllback) {
         })
         .then(function (data) {
             let today = new Date();
-            let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+            let date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
             document.getElementById('city-name').innerText = data.name;
             document.getElementById('current-date').innerText = date;
             document.getElementById('current-weather').innerText = "There is " + data.weather[0].description + " outside.";
@@ -26,6 +26,30 @@ function callWeather(calllback) {
             document.getElementById('current-wind').innerText = "Current wind speeds are " + data.wind.speed + "km/h.";
             document.getElementById('current-uv').innerText = "The UV index is:";
             document.getElementById('current-icon').innerHTML = "<img src=\"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png\">";
+        })
+        .catch(function (error) {
+            console.log('Request failed', error)
+        });
+
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Waterloo&appid=569731c1a4f5965656b996cf8ec76ae7&units=metric')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (i = 1; i < 6; i++) {
+                let jsonNum = i * 8 - 2;
+
+                let targetDate = new Date();
+                targetDate.setDate(targetDate.getDate() + i);
+                let date = (targetDate.getMonth() + 1) + '-' + targetDate.getDate() + '-' + targetDate.getFullYear();
+
+                document.getElementById('future-' + i + '-date').innerText = date;
+                document.getElementById('future-' + i + '-weather').innerText = "There is " + data.list[jsonNum].weather[0].description + " outside.";
+                document.getElementById('future-' + i + '-temp').innerText = "Temperature is " + data.list[jsonNum].main.temp + "°C";
+                document.getElementById('future-' + i + '-humidity').innerText = "Humidity is " + data.list[jsonNum].main.humidity + "%";
+                document.getElementById('future-' + i + '-wind').innerText = "Current wind speeds are " + data.list[jsonNum].wind.speed + "km/h.";
+                document.getElementById('future-' + i + '-icon').innerHTML = "<img src=\"http://openweathermap.org/img/w/" + data.list[jsonNum].weather[0].icon + ".png\">";
+            }
         })
         .catch(function (error) {
             console.log('Request failed', error)
