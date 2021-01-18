5c38caac11a0f0e3edb2196d4ce098ffaf5a9d5a
Terrence Chan <terrencechan.writer@gmail.com>
2021-01-17 23:39:35 -0500

    Finished loading in recent searches and UV index.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,18 @@
 var cityName = "Waterloo";
 var searchValue = document.getElementById('search');
+var initializeArray;
+var coords;
 
+// Initializes page
 var init = function () {
     if (localStorage.getItem('recent') == null) {
-        let initializeArray = [];
+        initializeArray = [];
         localStorage.setItem('recent', JSON.stringify(initializeArray));
     }
 
-    for (var x = 1; x < 6; x++) {
+    populateRecent();
+
+    for (x = 1; x < 6; x++) {
         let popItems = document.getElementById('pop-' + x);
         popItems.addEventListener('click', function () {
             cityName = popItems.innerText;
@@ -20,13 +25,17 @@ var init = function () {
         cityName = searchValue.value;
         console.log(cityName);
         callWeather();
+        addToRecent(cityName);
+        populateRecent();
     })
 
-    document.getElementById('search').addEventListener("keydown", event => {
+    document.getElementById('search').addEventListener("keydown", function () {
         if (event.key === 'Enter') {
             cityName = searchValue.value;
             console.log(cityName);
             callWeather();
+            addToRecent(cityName);
+            populateRecent();
         }
     });
 
@@ -41,6 +50,26 @@ function callWeather(calllback) {
             return response.json();
         })
         .then(function (data) {
+            // Fetch UV index
+            fetch('https://api.openweathermap.org/data/2.5/uvi?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&appid=569731c1a4f5965656b996cf8ec76ae7')
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (uvdata) {
+                    let currentUV = document.getElementById('current-uv');
+                    currentUV.innerText = "UV Index " + uvdata.value;
+                    if (uvdata.value <= 3) {
+                        currentUV.style.backgroundColor = "green";
+                    } else if (uvdata.value <= 6) {
+                        currentUV.style.backgroundColor = "orange";
+                    } else {
+                        currentUV.style.backgroundColor = "red";
+                    }
+                    return;
+                })
+                .catch(function (error) {
+                    console.log('Request failed', error)
+                });
             let today = new Date();
             let date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
             document.getElementById('city-name').innerText = data.name;
@@ -51,6 +80,7 @@ function callWeather(calllback) {
             document.getElementById('current-wind').innerText = "Current wind speeds are " + data.wind.speed + "km/h.";
             document.getElementById('current-uv').innerText = "The UV index is:";
             document.getElementById('current-icon').innerHTML = "<img src=\"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png\">";
+            return;
         })
         .catch(function (error) {
             console.log('Request failed', error)
@@ -82,6 +112,34 @@ function callWeather(calllback) {
         });
 }
 
+// Adds city to recent list
+function addToRecent(city) {
+    let recentItems = JSON.parse(localStorage.getItem('recent'));
+    if (recentItems.length > 4) {
+        recentItems = recentItems.slice(1);
+    }
+    recentItems.push(city);
+    localStorage.setItem('recent', JSON.stringify(recentItems));
+}
+
+// Populates recent list
+function populateRecent() {
+    var recentSearches = document.getElementById('recent-searches');
+    var recentData = JSON.parse(localStorage.getItem('recent'));
+
+    recentSearches.innerHTML = "";
+    for (let x = 0; x < 5; x++) {
+        if (recentData[x] !== undefined) {
+            var newList = document.createElement('li');
+            var newRecent = document.createTextNode(recentData[x]);
+            newList.appendChild(newRecent);
+            recentSearches.appendChild(newList).addEventListener('click', function () {
+                cityName = event.target.innerText;
+                callWeather();
+            });
+        }
+    }
+}
 
 window.onload = init;
 
